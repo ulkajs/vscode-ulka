347100f8f2c6b0cfd289b6f1805197e7bb640824
Roshan Acharya <acharyaroshan2357@gmail.com>
2021-12-11 16:15:54 +0545

    feat: added formatter

--- README.md
+++ README.md
@@ -2,15 +2,47 @@
     <img src="./images/logo.png"/>
 </p>
 
-<h2 align="center">Ulka Language Support</h2>
+<h2 align="center">Vscode Ulka</h2>
 
-<p align="center">Syntax highlight for <code>.ulka</code> files.</p>
+<p align="center">Syntax highlighter and Formatter for <code>.ulka</code> files.</p>
 
 ## Features
 
 - Highlight html in `.ulka` files.
 - Highlight js inside ulka block.
 - Highlight html inside template literals.
+- Highlight frontmatter yaml.
+- Format html and ulka block.
+
+## Settings
+
+- Add emmet support
+
+  ```json
+  {
+    "emmet.includeLanguages": {
+      "ulka": "html"
+    }
+  }
+  ```
+
+- Icon
+  - material-icon-theme
+    ```json
+    {
+      "material-icon-theme.files.associations": {
+        "*.ulka": "html"
+      }
+    }
+    ```
+  - vscode-icons
+    ```json
+    {
+      "vsicons.associations.files": [
+        { "icon": "html", "extensions": ["ulka"], "format": "svg" }
+      ]
+    }
+    ```
 
 ## Screenshot
 
---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import path from "path"
+import * as vscode from "vscode"
+import prettier, { Options } from "prettier"
+import ulkaFormat from "@ulkajs/format"
+
+export function activate(context: vscode.ExtensionContext) {
+  vscode.languages.registerDocumentFormattingEditProvider("ulka", {
+    provideDocumentFormattingEdits(document: vscode.TextDocument) {
+      return format(document)
+    },
+  })
+
+  vscode.languages.registerDocumentRangeFormattingEditProvider("ulka", {
+    provideDocumentRangeFormattingEdits(
+      document: vscode.TextDocument,
+      range: vscode.Range
+    ) {
+      return format(document, {
+        rangeEnd: document.offsetAt(range.end),
+        rangeStart: document.offsetAt(range.start),
+      })
+    },
+  })
+}
+
+const format = async (document: vscode.TextDocument, options: Options = {}) => {
+  let config: any =
+    vscode.workspace.getConfiguration("prettier", document.uri) || {}
+
+  config = {
+    ...config,
+    ...options,
+    ...(await prettier.resolveConfig(document.fileName, { useCache: false })),
+  }
+
+  const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)?.uri
+    .path
+
+  const ignored = workspacePath
+    ? await isIgnored(
+        document.fileName,
+        path.join(workspacePath, ".prettierignore")
+      )
+    : false
+
+  if (ignored) return []
+
+  const lastLineId = document.lineCount - 1
+  const range = new vscode.Range(
+    0,
+    0,
+    lastLineId,
+    document.lineAt(lastLineId).text.length
+  )
+
+  return [
+    vscode.TextEdit.replace(range, ulkaFormat(document.getText(), config)),
+  ]
+}
+
+const isIgnored = async (filepath: string, ignorePath?: string) => {
+  try {
+    return (await prettier.getFileInfo(filepath, { ignorePath })).ignored
+  } catch (err) {
+    return false
+  }
+}
