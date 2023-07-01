6768a1f0e1ee92ce964f3d10b3aedc751bacb4ab
Roshan Acharya <acharyaroshan2357@gmail.com>
2023-07-01 08:20:14 +0545

    removed support for old ulka-parser

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,14 @@
-import path from "path"
-import * as vscode from "vscode"
-import prettier, { Options } from "prettier"
-import ulkaFormat from "@ulkajs/format"
+import path from "path";
+import * as vscode from "vscode";
+import prettier, { Options } from "prettier";
+import ulkaFormat from "@ulkajs/format";
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.languages.registerDocumentFormattingEditProvider("ulka", {
     provideDocumentFormattingEdits(document: vscode.TextDocument) {
-      return format(document)
+      return format(document);
     },
-  })
+  });
 
   vscode.languages.registerDocumentRangeFormattingEditProvider("ulka", {
     provideDocumentRangeFormattingEdits(
@@ -18,50 +18,50 @@ export function activate(context: vscode.ExtensionContext) {
       return format(document, {
         rangeEnd: document.offsetAt(range.end),
         rangeStart: document.offsetAt(range.start),
-      })
+      });
     },
-  })
+  });
 }
 
 const format = async (document: vscode.TextDocument, options: Options = {}) => {
   let config: any =
-    vscode.workspace.getConfiguration("prettier", document.uri) || {}
+    vscode.workspace.getConfiguration("prettier", document.uri) || {};
 
   config = {
     ...config,
     ...options,
     ...(await prettier.resolveConfig(document.fileName, { useCache: false })),
-  }
+  };
 
   const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri)?.uri
-    .path
+    .path;
 
   const ignored = workspacePath
     ? await isIgnored(
         document.fileName,
         path.join(workspacePath, ".prettierignore")
       )
-    : false
+    : false;
 
-  if (ignored) return []
+  if (ignored) return [];
 
-  const lastLineId = document.lineCount - 1
+  const lastLineId = document.lineCount - 1;
   const range = new vscode.Range(
     0,
     0,
     lastLineId,
     document.lineAt(lastLineId).text.length
-  )
+  );
 
   return [
     vscode.TextEdit.replace(range, ulkaFormat(document.getText(), config)),
-  ]
-}
+  ];
+};
 
 const isIgnored = async (filepath: string, ignorePath?: string) => {
   try {
-    return (await prettier.getFileInfo(filepath, { ignorePath })).ignored
+    return (await prettier.getFileInfo(filepath, { ignorePath })).ignored;
   } catch (err) {
-    return false
+    return false;
   }
-}
+};
